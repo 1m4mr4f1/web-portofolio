375b253b7e375fd99b11789f952081f8ef800a69
1m4mr4f1 <imamrafi192005@gmail.com>
2025-05-02 00:49:10 +0700

    Initial commit - web-portofolio Laravel project

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/auth/redirect', [authController::class, "redirect"]);
+
+Route::get('/auth/callback', [authController::class, "callback"]);
+
